c81c4c8773b031e20036f55509dc8c9dd0a7707f
Izolentiy <nima.munkonov@gmail.com>
2023-05-23 02:32:00 +0700

    ЛБ5 В4 (рефакторинг)

--- lb_5/sketch/var_4.hpp
+++ lb_5/sketch/var_4.hpp
@@ -24,15 +24,25 @@
 namespace var_4 {
   void setup();
   void loop();
-  void set_rotation_speed();
+  void configure_leds();
+  void configure_motor();
 }
 
 Ultrasonic sonar(TRIG, ECHO);
 bool was_pressed = LOW;
 bool straight = false;
-bool started = false, rotated = false;
+
 byte speed = 0;
-uint32_t btn_timer = 0;
+uint32_t btn_timer = 0, rot_time = 0, rot_start = 0;
+
+struct mode {
+  const uint32_t time;
+  const byte speed;
+}
+m0 = { 0, 0 },
+m1 = { 3000, 255 },
+m2 = { 4000, 128 },
+m3 = { 5000, 64 };
 
 void var_4::setup() {
 
@@ -47,45 +57,53 @@ void var_4::setup() {
   pinMode(IN_1, OUTPUT);
   pinMode(IN_2, OUTPUT);
 
-  digitalWrite(LED_F, straight);
-  digitalWrite(LED_R, !straight);
+  configure_leds();
 }
 
 void var_4::loop() {
   uint16_t distance = sonar.read();
-  uint32_t timer = 0, rot_start = 0;
   
-  if (distance < 10) {
-    speed = 255; timer = 3000;
-  } else if (distance < 20) {
-    speed = 128; timer = 4000;
-  } else if (distance < 30) {
-    speed = 64; timer = 5000;
-  } else {
-    speed = 0; timer = 0;
+  if (speed == 0) {
+    // motor is not spinning
+    if (distance < 10) {
+      speed = 255;
+      rot_time = 3000;
+    } else if (distance < 20) {
+      speed = 128;
+      rot_time = 4000;
+    } else if (distance < 30) {
+      speed = 64;
+      rot_time = 5000;
+    } else {
+      speed = 0;
+      rot_time = 0;
+    }
+    
+    if (speed != 0) 
+      // start spinning, save start time
+      rot_start = millis();
   }
 
-  if (speed != 0 && !started) {
-    started = true;
-    rot_start = millis();
-  }
-  if (millis() - rot_start > timer) {
+  if (millis() - rot_start > rot_time)
     speed = 0;
-  }
-  set_rotation_speed();
+  configure_motor();
 
   bool pressed = digitalRead(BTN);
   if (!pressed && was_pressed && millis() - btn_timer > BTN_DELAY) {
     straight = !straight;
     btn_timer = millis();
-    digitalWrite(LED_F, straight);
-    digitalWrite(LED_R, !straight);
+    configure_leds();
     PRINTLN("Pressed");
   }
   was_pressed = pressed;
 }
 
-void var_4::set_rotation_speed() {
+void var_4::configure_leds() {
+  digitalWrite(LED_F, straight);
+  digitalWrite(LED_R, !straight);
+}
+
+void var_4::configure_motor() {
   analogWrite(IN_1, speed * straight);
   analogWrite(IN_2, speed * !straight);
 }
